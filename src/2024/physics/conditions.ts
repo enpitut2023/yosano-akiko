b05c1018b7dde3fdd39d2fe3d1bd5411eb2287c4
Ryosuke Takahashi <rjooske@gmail.com>
2025-12-24 10:29:52 +0900

    (WIP) new template

---
+++ src/2024/physics/conditions.ts
@@ -0,0 +1,25 @@
+import { CourseId, FakeCourseId, KnownCourse } from "../../akiko";
+import { SetupCreditRequirements } from "../../app-setup";
+
+export function classifyKnownCourses(cs: KnownCourse[]): Map<CourseId, string> {
+  const courseIdToCellId = new Map<CourseId, string>();
+  for (const c of cs) {
+    // TODO
+  }
+  return courseIdToCellId;
+}
+
+export function classifyRealCourses(): Map<CourseId, string> {
+  return new Map<CourseId, string>();
+}
+
+export function classifyFakeCourses(): Map<FakeCourseId, string> {
+  return new Map<FakeCourseId, string>();
+}
+
+export const creditRequirements: SetupCreditRequirements = {
+  cells: {},
+  columns: {},
+  compulsory: 0, // TODO
+  elective: 0, // TOD
+};
