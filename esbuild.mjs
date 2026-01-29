37763744969fc23ed34f0f0c1b1d76f7c03884d9
Ryosuke Takahashi <rjooske@gmail.com>
2026-01-29 12:16:42 +0900

    fix the font path situation

--- esbuild.mjs
+++ esbuild.mjs
@@ -39,7 +39,7 @@ async function main() {
 
       "src/app.scss",
       "src/styles.scss",
-      "src/docs/help.scss",
+      "src/docs/styles.scss",
     ],
     outbase: "src",
     bundle: true,
