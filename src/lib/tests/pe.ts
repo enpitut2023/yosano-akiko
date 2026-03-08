6cafdce32deca50f6ff785df58de6662545469c4
Ryosuke Takahashi <rjooske@gmail.com>
2026-03-08 22:07:36 +0900

    bring back tests

---
+++ src/lib/tests/pe.ts
@@ -0,0 +1,60 @@
+import {
+  classifyFakeCourses,
+  classifyRealCourses,
+  creditRequirementsSince2023,
+} from "@/requirements/pe-since-2023";
+import { runTest } from "./util";
+
+function test1(): void {
+  runTest({
+    csvPath: "grade-csvs/2023/pe-1.csv",
+    isNative: true,
+    creditRequirements: creditRequirementsSince2023,
+    classifyRealCourses: (cs, opts) => classifyRealCourses(cs, opts, 2023),
+    classifyFakeCourses: (cs, opts) => classifyFakeCourses(cs, opts, 2023),
+    want: {
+      cells: {
+        a3: { taken: 1 },
+        a4: { taken: 1, mightTake: 2 },
+        a5: { taken: 2 },
+        b1: { taken: 6 },
+        b2: { mightTake: 3 },
+        b3: { taken: 2, mightTake: 2 },
+        c2: { taken: 1 },
+        c3: { taken: 1 },
+        c4: { taken: 1 },
+        c5: { taken: 1 },
+        c6: { taken: 1 },
+        d1: { taken: 9 },
+        d2: { taken: 4 },
+        d3: { rawTaken: 12, effectiveTaken: 10 },
+        d4: { taken: 6 },
+        d5: { taken: 1 },
+        e1: { taken: 1 },
+        e2: { taken: 1 },
+        e3: { taken: 3 },
+        e4: { taken: 4 },
+        e5: { taken: 2 },
+        h1: {
+          rawTaken: 22,
+          effectiveTaken: 20,
+          rawMightTake: 4,
+          effectiveMightTake: 0,
+        },
+      },
+      columns: {
+        a: { taken: 4, mightTake: 2 },
+        b: { taken: 8, mightTake: 5 },
+        c: { taken: 5 },
+        d: { taken: 30 },
+        e: { taken: 11 },
+        h: { taken: 20 },
+      },
+      compulsory: { taken: 20, mightTake: 2 },
+      elective: { taken: 58, mightTake: 5 },
+    },
+  });
+}
+
+test1();
+console.log(import.meta.filename, "ok");
---
+++ src/lib/tests/physics.ts
@@ -0,0 +1,63 @@
+import {
+  classifyFakeCourses,
+  classifyRealCourses,
+  creditRequirementsSince2023,
+} from "@/requirements/physics-since-2023";
+import { runTest } from "./util";
+
+function test1(): void {
+  runTest({
+    csvPath: "grade-csvs/2023/physics-1.csv",
+    isNative: false,
+    creditRequirements: creditRequirementsSince2023,
+    classifyRealCourses: (cs, opts) => classifyRealCourses(cs, opts, 2023),
+    classifyFakeCourses: (cs, opts) => classifyFakeCourses(cs, opts, 2023),
+    want: {
+      cells: {
+        a1: { taken: 2 },
+        a2: { mightTake: 6 },
+        a4: { taken: 3 },
+        b1: { taken: 8, mightTake: 3 },
+        b2: { taken: 5, mightTake: 3 },
+        b3: { taken: 6 },
+        b4: { taken: 22, mightTake: 2 },
+        d1: { taken: 7 },
+        d2: { taken: 8 },
+        d3: { taken: 15.5 },
+        e1: { taken: 2 },
+        e2: { taken: 2 },
+        e3: { taken: 4 },
+        e4: { taken: 4 },
+        f1: { taken: 1 },
+        h1: { taken: 8 },
+        h2: {
+          rawTaken: 20,
+          effectiveTaken: 18,
+          rawMightTake: 2,
+          effectiveMightTake: 0,
+        },
+      },
+      columns: {
+        a: { taken: 5, mightTake: 6 },
+        b: { taken: 41, mightTake: 8 },
+        d: { taken: 30.5 },
+        e: { taken: 12 },
+        f: { taken: 1 },
+        h: {
+          rawTaken: 26,
+          effectiveTaken: 24,
+        },
+      },
+      compulsory: { taken: 17, mightTake: 6 },
+      elective: {
+        rawTaken: 96.5,
+        effectiveTaken: 91,
+        rawMightTake: 8,
+        effectiveMightTake: 0,
+      },
+    },
+  });
+}
+
+test1();
+console.log(import.meta.filename, "ok");
